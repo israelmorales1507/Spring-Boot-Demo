b8f528c1d7fa27ec3e58a304ec842af49854172d
Israel Morales <israel.morales1507@hotmail.com>
2023-10-22 14:52:08 -0600

    Errores

--- 06-spring-boot-spring-mvc/02-spring-boot-validation/src/main/java/com/example/springdemo/mvc/model/Customer.java
+++ 06-spring-boot-spring-mvc/02-spring-boot-validation/src/main/java/com/example/springdemo/mvc/model/Customer.java
@@ -1,7 +1,6 @@
 package com.example.springdemo.mvc.model;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -11,11 +10,37 @@ public class Customer {
     @Size(min = 1, message = "is required")
     private String lastName;
 
+    @NotNull(message = "is required")
+    @Min(value = 0, message = "The value most be greater than or equals to zero")
+    @Max(value = 10, message = "The value most be less than or equals to 10")
+    private Integer freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
+
     public Customer(){}
 
-    public Customer(String firstName, String lastName) {
+    public Customer(String firstName, String lastName, int freePasses, String postalCode) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.freePasses = freePasses;
+        this.postalCode = postalCode;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public Integer getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(Integer freePasses) {
+        this.freePasses = freePasses;
     }
 
     public String getFirstName() {
